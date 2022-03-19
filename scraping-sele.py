9825ffba91c0f1fbdc08d0fc406c8936a0bc6816
fardantif21 <fardan.al.tif421@polban.ac.id>
2022-03-20 00:03:49 +0700

    file web scraping (selenium)

--- scraping-sele.py
+++ scraping-sele.py
@@ -11,42 +11,45 @@ time = datetime.now()
 
 ListSmartphone = []
 
-
-for smartphone in driver.find_elements_by_tag_name("tbody"):
-    for data in smartphone.find_elements_by_tag_name("tr"):
-        for img in data.find_elements_by_class_name("prod-img"):
-            search = 0
-            found = False
-            if(len(data.text.split()) > 5):
-                while(found == False):
-                    search = search + 1
-                    if(data.text.split()[search] == '-'):
-                        found = True
-                    elif(data.text.split()[search] == 'Phone'):
-                        found = True
-                    elif(data.text.split()[search] == 'K40'):
-                        found = True
-                
-                    idx = 1
-                    model = data.text.split()[idx] + ' '
-                    idx = idx + 1
-                    while(idx<search+3):
-                        model = model + data.text.split()[idx] + ' '
-                        idx = idx + 1
-        
+i = 1
+while i<=100:
+    for smartphone in driver.find_elements_by_tag_name("tbody"):
+        for data in smartphone.find_elements_by_tag_name("tr"):
+            for img in data.find_elements_by_class_name("prod-img"):
+                urllib.request.urlretrieve(img.get_attribute("src"), str(i)+".png")
+                i = i + 1
+                search = 0
+                found = False
                 if(len(data.text.split()) > 5):
-                    idx = search+3
-                    processor = data.text.split()[idx] + ' ' + data.text.split()[idx+1]
+                    while(found == False):
+                        search = search + 1
+                        if(data.text.split()[search] == '-'):
+                            found = True
+                        elif(data.text.split()[search] == 'Phone'):
+                            found = True
+                        elif(data.text.split()[search] == 'K40'):
+                            found = True
                 
-            if(len(data.text.split()) > 5):
-                ListSmartphone.append(
-                    {"Rank": data.text.split("\n")[0],
-                     "Image": img.get_attribute("src"),
-                     "Model": model,
-                     "Processor": processor,
-                     "Waktu_Scraping": time.strftime("%Y-%m-%d %H:%M:%S"),
-                    }
-                )
+                        idx = 1
+                        model = data.text.split()[idx] + ' '
+                        idx = idx + 1
+                        while(idx<search+3):
+                            model = model + data.text.split()[idx] + ' '
+                            idx = idx + 1
+        
+                    if(len(data.text.split()) > 5):
+                        idx = search+3
+                        processor = data.text.split()[idx] + ' ' + data.text.split()[idx+1]
+                
+                if(len(data.text.split()) > 5):
+                    ListSmartphone.append(
+                        {"Rank": data.text.split("\n")[0],
+                         "Image": img.get_attribute("src"),
+                         "Model": model,
+                         "Processor": processor,
+                         "Waktu_Scraping": time.strftime("%Y-%m-%d %H:%M:%S"),
+                        }
+                    )
 
 hasil_scraping = open("hasilscraping-2.json", "w")
 json.dump(ListSmartphone, hasil_scraping, indent = 6)
